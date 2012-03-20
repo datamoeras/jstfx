c6c79fa2a24f6cc06d33d1d01de1f058944c7bfa
Raoul Zwart <datamoeras@gmail.com>
2012-03-20 11:43:52 +0100

    fork me on gh

--- voicesrv.pl
+++ voicesrv.pl
@@ -8,6 +8,7 @@ use voicesrv;
 
 #voicesrv->new->request('~ja:+++***[1.4C#4,1.4A5,2.4C#4,1.4F#4]hallo toshiba');
 #voicesrv->new->request('toch');
-voicesrv->new->request('ja: @ * + Q sakura * yu');
+#voicesrv->new->request('ja: @ * + Q sakura * yu');
+voicesrv->new->request('ja:@ * + Q sakura * yu');
 
 
